538211e9e01cec4b9651f36c8e1efad7958fe060
Brandon Castro <castroyou999@gmail.com>
2023-02-05 23:01:18 -0500

    commit tree 56feb656805595654d1d55db035cd007f2a2904a

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import styled from "styled-components";
 
 function MovieForm() {
+  const [errors , setErrors] = useState([])
   const [formData, setFormData] = useState({
     title: "",
     year: new Date().getFullYear(),
@@ -14,18 +15,42 @@ function MovieForm() {
     female_director: false,
   });
 
-  function handleSubmit(e) {
+  // function handleSubmit(e) {
+  //   e.preventDefault();
+  //   fetch("/movies", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(formData),
+  //   })
+  //     .then((response) => {
+  //       if (response.ok){
+  //         response.json().then((newMovie) => console.log(newMovie))
+  //       } else {
+  //         response.json().then((errorData) => setErrors(errorData.errors))
+  //       }
+  //     })
+  // }
+
+  async function handleSubmit(e) {
     e.preventDefault();
-    fetch("/movies", {
+    const response = await fetch("/movies", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(formData),
     })
-      .then((response) => response.json())
-      .then((newMovie) => console.log(newMovie));
+    const data = await response.json();
+    if (response.ok){
+      console.log("Movie created:", newMovie);
+    } else {
+      setErrors(data.errors);
+    }
   }
+  
+
 
   function handleChange(e) {
     const value =
@@ -125,6 +150,13 @@ function MovieForm() {
             />
           </label>
         </FormGroup>
+        {errors.length > 0 && (
+          <ul style={{color:"red"}}>
+            {errors.map((error) => (
+              <li key={error} >{error}</li>
+            ))}
+          </ul>
+        )}
         <SubmitButton type="submit">Add Movie</SubmitButton>
       </form>
     </Wrapper>
